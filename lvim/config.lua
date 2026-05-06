1aaadef30e00fe46fb745e982332a1feeccdad0a
Stephen SooHoo <stephen+github@soohoo.io>
2026-05-05 21:45:14 -0500

    add page forward and map keymaps with auto-center

--- lvim/config.lua
+++ lvim/config.lua
@@ -11,6 +11,8 @@ vim.opt.expandtab = true
 -- custom page movement keymaps from ThePrimeagean
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<S-p>", [["_dP]]) -- copy back pasted item into buffer!
